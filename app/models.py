2ac627719546e31fb653729c6c24ab65f6928198
SamRus <semarus10@mail.ru>
2020-10-21 22:13:47 +0300

    second

--- app/models.py
+++ app/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Some(models.Model):
-    name = models.ChartField(max_length=10)
+    name = models.TextField(max_length=10)
+    love = models.TextField()
