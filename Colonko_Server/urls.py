0c39e0f45b9dadbb4afd5e6f95493083158db0a9
SamRus <semarus10@mail.ru>
2020-12-05 19:13:13 +0300

    fix

--- Colonko_Server/urls.py
+++ Colonko_Server/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 from app import views
 from django.views.decorators.csrf import csrf_exempt
 
@@ -32,4 +34,4 @@ urlpatterns = [
     #-------------------------------------------------------
 
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
